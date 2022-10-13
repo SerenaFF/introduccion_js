73633bb86f10221fb32d9f4a3e42095d6db91479
SerenaFF <serena.ff@outlook.com>
2022-10-13 00:46:08 -0300

    added new files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# introduccion_js
+# Desafío: javaScript
+
+## Nombre de Desafío: introduccion_js
+
+## DESAFIO COMPLETADO
+
+Crea una lista con los siguientes elementos:
+
+- Tu nombre (string)
+- Tu apellido (string)
+- Tu edad (number)
+- ¿Eres desarrollador de aplicaciones web? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+- Tu pasatiempos favoritos (Array)
+- Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar
---
+++ main.js
@@ -0,0 +1,49 @@
+// Desafío: javaScript
+
+// Nombre de Desafío: introduccion_js
+
+// DESAFIO COMPLETADO
+
+// Crea una lista con los siguientes elementos:
+
+// Tu nombre (string)
+var nombre = "Serena"
+console.log(nombre)
+
+// Tu apellido (string)
+var apellido = "Fernandez"
+console.log(apellido)
+
+// Tu edad (number)
+var edad = 21
+console.log(edad)
+
+// ¿Eres desarrollador de aplicaciones web? (boolean)
+var desarrolladora = true
+console.log(desarrolladora)
+
+// Tu fecha de nacimiento (Date)
+var nacimiento = "2001-4-27"
+console.log(nacimiento)
+
+// Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+var libroFavorito = {
+    titulo: "Proyect Hail Mary",
+    autor: "Andy Weir",
+    fecha: "2021-5-4",
+    url: "https://www.goodreads.com/book/show/54493401-project-hail-mary"
+};
+console.log(libroFavorito)
+
+// Tu pasatiempos favoritos (Array)
+var pasatiempos = [
+    "Gaming",
+    "Coding",
+    "Leer",
+    "Salir con amigos",
+];
+console.log(pasatiempos)
+
+// Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar
+const miActividadFavorita = "Jugar en la PC";
+console.log(miActividadFavorita)
